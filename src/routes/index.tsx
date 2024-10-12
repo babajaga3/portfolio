ca36fee43f5a4c14aa8439c00d0d9127bc984653
Toma Bourov <t.bourov@gmail.com>
2024-10-12 03:22:36 +0300

    :zap: :zap: Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,28 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { Code } from 'iconoir-react'
+import { Globe, SocialBar } from '@/components/ui'
+
+export const Route = createFileRoute('/')({
+  component: Index
+})
+
+function Index() {
+
+  return (
+    <div className={'grid grid-cols-2'}>
+      <div className={'bg-theme-foreground'}>
+        <h1 className={'text-theme-accent text-5xl font-bold p-8'}>
+          What&apos;s cooking, good looking?
+        </h1>
+        <div className={'flex flex-row gap-2 pl-12'}>
+          <Code />
+          <h3 className={'text-theme-accent flex flex-row gap-1.5'}>
+            Frontend React Engineer
+          </h3>
+        </div>
+        <SocialBar />
+      </div>
+      <Globe/>
+    </div>
+  )
+}
