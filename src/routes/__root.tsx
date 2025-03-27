dab00f69c969c50aac44da952463e52f56efbdba
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-03-27 17:40:42 +0200

    :zap: Add constellation to bg

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,29 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { cn } from '@/lib/utils'
+import { Constellation } from '@/components/custom/constellation'
 
 export const Route = createRootRoute({
-  component: () => (
-    <div className={'overflow-hidden bg-theme-background font-mono'}>
-      <Outlet />
-    </div>
-  )
+  component: () => {
+    const globalCn = cn([
+      // 'text-theme-accent-two',
+      // 'bg-theme-background-two',
+      'font-NeueMaksima',
+      'px-8 sm:px-16',
+      'py-12',
+      'h-screen',
+      'overflow-auto',
+      'relative'
+    ])
+    
+    return (
+      <div className={globalCn}>
+        <div className="absolute inset-0 -z-10 bg-linear-to-t from-transparent to-black pointer-events-none">
+          <Constellation />
+        </div>
+        <div className='relative z-10'>
+          <Outlet />  
+        </div>
+      </div>
+    )
+  }
 })
