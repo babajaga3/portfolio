603f3c344803bfd12f2a8ca3df1baae490e63d18
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-03-27 15:47:34 +0200

    :wrench: Add SVGR to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import svgr from 'vite-plugin-svgr'
 import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
       react(),
-      TanStackRouterVite()
+      TanStackRouterVite(),
+      svgr()
   ],
   resolve: {
     alias: {
