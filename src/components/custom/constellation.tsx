a3fb054e8d3d2dfc90d66ac26b7b6f762d9cb468
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-03-27 17:17:58 +0200

    :zap: Create constellation effect component

---
+++ src/components/custom/constellation.tsx
@@ -0,0 +1,107 @@
+import Particles, { initParticlesEngine } from '@tsparticles/react'
+import { loadSlim } from '@tsparticles/slim';
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+import { useEffect, useState, useMemo } from 'react'
+
+export function Constellation() {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+  
+    const particlesLoaded = async (container?: Container): Promise<void> => {
+      console.log(container);
+    };
+  
+    const options: ISourceOptions = useMemo(
+      () => ({
+        background: {
+          color: {
+            value: "#0A0A0A",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "grab",
+            },
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          move: {
+            direction: MoveDirection.none,
+            enable: true,
+            outModes: {
+              default: OutMode.out,
+            },
+            random: true,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+            },
+            value: 300,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 3 },
+          },
+        },
+        detectRetina: true
+      }),
+      [],
+    );
+  
+    if (init) {
+      return (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+        />
+      );
+    }
+  
+    return <></>;
+}
